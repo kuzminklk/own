4975712a486d0093aa178ecf299ea307881e14f8
kuzminkdk <kuzminkdk@gmail.com>
2026-08-16 00:13:18 +0300

    Update commands.sh

--- programms/age/commands.sh
+++ programms/age/commands.sh
@@ -2,7 +2,9 @@
 # Encrypt
 age --passphrase -o sensitive-encrypted.txt sensitive.md
 age --passphrase -o seed-phrases-encrypted.txt seed-phrases.md
+age --passphrase -o documents-encrypted.txt documents.md
 
 # Decrypt
 age --decrypt -o sensitive.md sensitive-encrypted.txt
 age --decrypt -o seed-phrases.md seed-phrases-encrypted.md
+age --decrypt -o documents.md documents-encrypted.txt
